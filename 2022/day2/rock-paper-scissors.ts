916c4716bc9e597508b125e43eba79d98fc8c6f0
Lars Loonen <lars.van.der.spek@vandoren.nl>
2023-09-16 22:28:55 +0200

    Solved day 2 part 2

--- 2022/day2/rock-paper-scissors.ts
+++ 2022/day2/rock-paper-scissors.ts
@@ -18,39 +18,39 @@ function determineScore(opponentMove : string, myMove: string) : number {
 	switch(opponentMove)
 	{
 		case "A": // Rock
-			if (myMove === "X") { // Rock 
+			if (myMove === "X") { // Lose: Scissors 
+				score += 3;
+				score += 0;
+			} else if (myMove === "Y") { // Draw: Rock 
 				score += 1;
 				score += 3;
-			} else if (myMove === "Y") { // Paper 
+			} else if (myMove === "Z") { // Win: Paper 
 				score += 2;
 				score += 6;
-			} else if (myMove === "Z") { // Scissors
-				score += 3
-				score += 0;
 			}
 			break;
 		case "B": // Paper
-			if (myMove === "X") { // Rock 
+			if (myMove === "X") { // Lose: Rock
 				score += 1;
 				score += 0;
-			} else if (myMove === "Y") { // Paper 
+			} else if (myMove === "Y") { // Draw: Paper
 				score += 2;
 				score += 3;
-			} else if (myMove === "Z") { // Scissors
-				score += 3
+			} else if (myMove === "Z") { // Win: Scissors
+				score += 3;
 				score += 6;
 			}
 			break;
 		case "C": // Scissors
-			if (myMove === "X") { // Rock 
-				score += 1;
-				score += 6;
-			} else if (myMove === "Y") { // Paper 
+			if (myMove === "X") { // Lose: Paper
 				score += 2;
 				score += 0;
-			} else if (myMove === "Z") { // Scissors
-				score += 3
+			} else if (myMove === "Y") { // Draw: Scissors
 				score += 3;
+				score += 3;
+			} else if (myMove === "Z") { // Win: Rock
+				score += 1;
+				score += 6;
 			}
 			break;
 
