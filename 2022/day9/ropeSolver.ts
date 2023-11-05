6d1e7d101e38b7da046f3af657ad492cf870f8ae
Lars Loonen <lars.van.der.spek@vandoren.nl>
2023-11-05 21:55:54 +0100

    solved day 9 part 1

--- 2022/day9/ropeSolver.ts
+++ 2022/day9/ropeSolver.ts
@@ -8,12 +8,13 @@ type Point = {
     y: number;
 };
 
+let headVisits: Point[] = [];
 let tailVisits: Point[] = [];
 let currHead: Point = { x : 0, y : 0 };
 let currTail: Point = { x : 0, y : 0 };
 tailVisits.push(currTail);
 
-for (let move of moves.slice(0, 3)) {
+for (let move of moves) {
 
     if (!move) { continue; }
 
@@ -37,13 +38,19 @@ for (let move of moves.slice(0, 3)) {
 
     for (let i = 0; i < steps; i++) {
         const newHead = { x: currHead.x + dir[0], y: currHead.y + dir[1] };
+        headVisits.push(newHead);
+        console.log("head: ", headVisits[headVisits.length-1]);
         currHead = newHead;
 
         let newTail = walkTail(currHead, currTail);
-        // check for distinct point before pushing.
-        tailVisits.push(newTail);
+
+        var distinct = tailVisits.findIndex(p => (p.x == newTail.x && p.y === newTail.y));
+        if(distinct <= -1){
+            tailVisits.push(newTail);
+        }        
         currTail = newTail;
-        console.log(tailVisits[tailVisits.length-1]);
+        console.log("tail: ", tailVisits[tailVisits.length-1]);
+        console.log("Number of tail visits: ", tailVisits.length);
     }
 }
 
@@ -51,26 +58,57 @@ function walkTail(headPoint: Point, tailPoint: Point): Point {
 
     let head = [headPoint.x, headPoint.y];
     let tail = [tailPoint.x, tailPoint.y];
-    // subtract the two points
+    // subtract the head and tail 
     let diff = head.map((hVal, hIdx) => hVal - tail[hIdx]);
-    console.log(diff);
 
+    let newTailPoint : Point = { x: tail[0], y: tail[1] };
     if (diff[0] <= 1 && 
         diff[0] >= -1 &&
         diff[1] <= 1 &&
         diff[1] >= -1) {
-        return tailPoint;
+        return newTailPoint;
     }
-    else if (diff[0] > 1) {
-        // check for diff[1]
+
+    if (diff[0] > 1) {
+        newTailPoint.x++;
+
+        if (diff[1] > 0) {
+            newTailPoint.y++;
+        }
+        else if (diff[1] < 0) {
+            newTailPoint.y--;
+        }
     }
     else if (diff[0] < -1) {
-        // check for diff[1]
+        newTailPoint.x--;
+
+        if (diff[1] > 0) {
+            newTailPoint.y++;
+        }
+        else if (diff[1] < 0) {
+            newTailPoint.y--;
+        }
     }
     else if (diff[1] > 1) {
-        // check for diff[0]
+        newTailPoint.y++;
+
+        if (diff[0] > 0) {
+            newTailPoint.x++;
+        }
+        else if (diff[0] < 0) {
+            newTailPoint.x--;
+        }
     }
     else if (diff[1] < -1) {
-        // check for diff[0]
+        newTailPoint.y--;
+
+        if (diff[0] > 0) {
+            newTailPoint.x++;
+        }
+        else if (diff[0] < 0) {
+            newTailPoint.x--;
+        }
     }
+
+    return newTailPoint;
 }
