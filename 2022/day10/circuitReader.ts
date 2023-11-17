2cf31f23c347cb74af80dd25293ecaf8cafd8038
Lars Loonen <lars.van.der.spek@vandoren.nl>
2023-11-17 21:23:30 +0100

    Solved day 10 part 1

--- 2022/day10/circuitReader.ts
+++ 2022/day10/circuitReader.ts
@@ -1,4 +1,41 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("day10/input.txt", "utf-8");
-let instructions = input.split("\r\n");
+const inputText = fs.readFileSync("day10/input.txt", "utf-8");
+let instructions = inputText.split("\r\n");
+
+let registerValue = 1;
+let cycles = 0;
+let signalStrength = 0;
+
+for(var instruction of instructions) {
+
+    //console.log(instruction);
+
+    if (instruction.startsWith("noop")) {
+        increaseCycle();
+    }
+    else if (instruction.startsWith("addx")) {
+        increaseCycle();
+        increaseCycle();
+        let addValue = Number(instruction.split(' ')[1]);
+        //console.log(addValue);
+        registerValue = registerValue + addValue;
+    }
+}
+
+console.log("register X: ", registerValue);
+console.log("cycles: ", cycles);
+console.log("signal strength: ", signalStrength);
+
+function increaseCycle() {
+    cycles++;
+
+    if (cycles === 20 || 
+        cycles === 60 ||
+        cycles === 100 ||
+        cycles === 140 ||
+        cycles === 180 ||
+        cycles === 220) {
+        signalStrength += registerValue * cycles;
+    }
+}
