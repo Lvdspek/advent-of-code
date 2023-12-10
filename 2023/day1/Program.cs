ce13fcad9bdbc8c29098029eabf16bd404d1262b
Lars Loonen <lars.van.der.spek@vandoren.nl>
2023-12-10 11:22:58 +0100

    Started on 2023 day1

---
+++ 2023/day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using (var sr = new StreamReader("day1/input.txt"))
+{
+    string? line = sr.ReadLine();
+    while (!string.IsNullOrEmpty(line)) 
+    {
+        line = sr.ReadLine();
+        var numbers = string.Concat( line.Where( Char.IsDigit ) );
+
+        int number;
+        if (numbers.Count() == 1) {
+            number = (int) numbers[0]^2;
+        }
+        else if (numbers.Count() == 2) {
+            number = (int)numbers[0] + (int)numbers[1];
+        }
+        else if (numbers.Count() > 2) {
+            numberSum = numbers.Select(x => (int)x).Sum();
+            Console.WriteLine(number);
+    }
+}
