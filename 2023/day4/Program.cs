63c14e9e1ed5fc8c3d145a2f468cfb9e2f6a28ff
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-01-15 14:11:03 +0100

    Started on day 4 part 2

--- 2023/day4/Program.cs
+++ 2023/day4/Program.cs
@@ -6,7 +6,6 @@ using (var sr = new StreamReader("input.txt"))
 
     while (line != null)
     {
-
         string cardNumbers = line.Split(":")[1];
         string winningNumbersString = cardNumbers.Split("|")[0];
         string ownNumbersString = cardNumbers.Split("|")[1];
@@ -16,7 +15,6 @@ using (var sr = new StreamReader("input.txt"))
                 .Where(x => int.TryParse(x, out _))
                 .Select(x => int.Parse(x))
                 .ToArray();
-
 
         int[] ownNumbers =
             ownNumbersString.Split(" ")
@@ -29,22 +27,18 @@ using (var sr = new StreamReader("input.txt"))
                 .Intersect(ownNumbers)
                 .ToArray();
 
-        // Console.WriteLine(string.Join(",", winningNumbers));
-        // Console.WriteLine(string.Join(",", ownNumbers));
-        // Console.WriteLine(string.Join(",", matches));
         int scoreAddition = 0;
-        for(int i = 0; i < matches.Length; i++) {
-            if (scoreAddition == 0) { 
-                scoreAddition = 1;
-            }
-            else {
-                scoreAddition += scoreAddition;
-            }
-        }
-        totalScore += scoreAddition;
-        Console.WriteLine($"Score addition: {scoreAddition}");
 
-        line = sr.ReadLine();      
+        for (int i = 0; i < matches.Length; i++)
+        {
+            scoreAddition = scoreAddition == 0
+                ? 1
+                : scoreAddition * 2;
+        }
+
+        totalScore += scoreAddition;
+
+        line = sr.ReadLine();
     }
 
     Console.WriteLine($"Total score: {totalScore}");
