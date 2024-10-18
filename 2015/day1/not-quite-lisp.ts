2a7982b09dd9b0baa6fdd1212ec1676343f60302
Lars Loonen <larsvdspek@gmail.com>
2024-10-18 22:31:28 +0200

    solved day1 part2

--- 2015/day1/not-quite-lisp.ts
+++ 2015/day1/not-quite-lisp.ts
@@ -4,10 +4,15 @@ const data = fs.readFileSync("2015/day1/input.txt", "utf-8");
 
 let up = 0;
 let down = 0;
+let count = 0;
 
 for (var char of data) {
+  count++;
+
   if (char === '(') up++
   else if (char === ')') { down++ }
+
+  if (up - down < 0) break;
 }
 
-console.log('end floor: ', up - down);
+console.log('first basement arrival: ', count);
